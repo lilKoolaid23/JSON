a785492e54132024d51e720f241d7e58fdfd04fb
lilKoolaid23 <90406402+lilKoolaid23@users.noreply.github.com>
2022-04-20 09:27:21 -0700

    Add files via upload

---
+++ JsonPokemon/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace JsonPokemon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nametextBox = new System.Windows.Forms.TextBox();
+            this.typetextBox = new System.Windows.Forms.TextBox();
+            this.movetextBox = new System.Windows.Forms.TextBox();
+            this.HPtextBox = new System.Windows.Forms.TextBox();
+            this.HeighttextBox = new System.Windows.Forms.TextBox();
+            this.Weighttextbox = new System.Windows.Forms.TextBox();
+            this.pokemonpictureBox = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.previousbutton = new System.Windows.Forms.Button();
+            this.Savebutton = new System.Windows.Forms.Button();
+            this.AddNewbutton = new System.Windows.Forms.Button();
+            this.Nextbutton = new System.Windows.Forms.Button();
+            this.firstbutton = new System.Windows.Forms.Button();
+            this.loadbutton = new System.Windows.Forms.Button();
+            this.deletebutton = new System.Windows.Forms.Button();
+            this.lastbutton = new System.Windows.Forms.Button();
+            this.descriptiontextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pokemonpictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(343, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pokedex";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Type";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Move";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "HP";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Height";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 254);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Weight";
+            // 
+            // nametextBox
+            // 
+            this.nametextBox.Location = new System.Drawing.Point(67, 107);
+            this.nametextBox.Name = "nametextBox";
+            this.nametextBox.Size = new System.Drawing.Size(100, 20);
+            this.nametextBox.TabIndex = 7;
+            // 
+            // typetextBox
+            // 
+            this.typetextBox.Location = new System.Drawing.Point(67, 136);
+            this.typetextBox.Name = "typetextBox";
+            this.typetextBox.Size = new System.Drawing.Size(100, 20);
+            this.typetextBox.TabIndex = 8;
+            // 
+            // movetextBox
+            // 
+            this.movetextBox.Location = new System.Drawing.Point(67, 165);
+            this.movetextBox.Name = "movetextBox";
+            this.movetextBox.Size = new System.Drawing.Size(100, 20);
+            this.movetextBox.TabIndex = 9;
+            // 
+            // HPtextBox
+            // 
+            this.HPtextBox.Location = new System.Drawing.Point(67, 195);
+            this.HPtextBox.Name = "HPtextBox";
+            this.HPtextBox.Size = new System.Drawing.Size(100, 20);
+            this.HPtextBox.TabIndex = 10;
+            // 
+            // HeighttextBox
+            // 
+            this.HeighttextBox.Location = new System.Drawing.Point(67, 221);
+            this.HeighttextBox.Name = "HeighttextBox";
+            this.HeighttextBox.Size = new System.Drawing.Size(100, 20);
+            this.HeighttextBox.TabIndex = 11;
+            // 
+            // Weighttextbox
+            // 
+            this.Weighttextbox.Location = new System.Drawing.Point(67, 247);
+            this.Weighttextbox.Name = "Weighttextbox";
+            this.Weighttextbox.Size = new System.Drawing.Size(100, 20);
+            this.Weighttextbox.TabIndex = 12;
+            // 
+            // pokemonpictureBox
+            // 
+            this.pokemonpictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pokemonpictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pokemonpictureBox.Image")));
+            this.pokemonpictureBox.Location = new System.Drawing.Point(200, 70);
+            this.pokemonpictureBox.Name = "pokemonpictureBox";
+            this.pokemonpictureBox.Size = new System.Drawing.Size(343, 257);
+            this.pokemonpictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pokemonpictureBox.TabIndex = 13;
+            this.pokemonpictureBox.TabStop = false;
+            this.pokemonpictureBox.Click += new System.EventHandler(this.pokemonpictureBox_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(637, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Description";
+            // 
+            // previousbutton
+            // 
+            this.previousbutton.Location = new System.Drawing.Point(200, 341);
+            this.previousbutton.Name = "previousbutton";
+            this.previousbutton.Size = new System.Drawing.Size(79, 24);
+            this.previousbutton.TabIndex = 15;
+            this.previousbutton.Text = "Previous";
+            this.previousbutton.UseVisualStyleBackColor = true;
+            this.previousbutton.Click += new System.EventHandler(this.previousbutton_Click);
+            // 
+            // Savebutton
+            // 
+            this.Savebutton.Location = new System.Drawing.Point(297, 341);
+            this.Savebutton.Name = "Savebutton";
+            this.Savebutton.Size = new System.Drawing.Size(79, 24);
+            this.Savebutton.TabIndex = 16;
+            this.Savebutton.Text = "Save";
+            this.Savebutton.UseVisualStyleBackColor = true;
+            this.Savebutton.Click += new System.EventHandler(this.Savebutton_Click);
+            // 
+            // AddNewbutton
+            // 
+            this.AddNewbutton.Location = new System.Drawing.Point(394, 341);
+            this.AddNewbutton.Name = "AddNewbutton";
+            this.AddNewbutton.Size = new System.Drawing.Size(79, 24);
+            this.AddNewbutton.TabIndex = 17;
+            this.AddNewbutton.Text = "Add New";
+            this.AddNewbutton.UseVisualStyleBackColor = true;
+            this.AddNewbutton.Click += new System.EventHandler(this.AddNewbutton_Click);
+            // 
+            // Nextbutton
+            // 
+            this.Nextbutton.Location = new System.Drawing.Point(489, 341);
+            this.Nextbutton.Name = "Nextbutton";
+            this.Nextbutton.Size = new System.Drawing.Size(79, 24);
+            this.Nextbutton.TabIndex = 18;
+            this.Nextbutton.Text = "Next";
+            this.Nextbutton.UseVisualStyleBackColor = true;
+            this.Nextbutton.Click += new System.EventHandler(this.Nextbutton_Click);
+            // 
+            // firstbutton
+            // 
+            this.firstbutton.Location = new System.Drawing.Point(200, 382);
+            this.firstbutton.Name = "firstbutton";
+            this.firstbutton.Size = new System.Drawing.Size(79, 24);
+            this.firstbutton.TabIndex = 19;
+            this.firstbutton.Text = "First";
+            this.firstbutton.UseVisualStyleBackColor = true;
+            this.firstbutton.Click += new System.EventHandler(this.firstbutton_Click);
+            // 
+            // loadbutton
+            // 
+            this.loadbutton.Location = new System.Drawing.Point(297, 382);
+            this.loadbutton.Name = "loadbutton";
+            this.loadbutton.Size = new System.Drawing.Size(79, 24);
+            this.loadbutton.TabIndex = 20;
+            this.loadbutton.Text = "Load";
+            this.loadbutton.UseVisualStyleBackColor = true;
+            this.loadbutton.Click += new System.EventHandler(this.loadbutton_Click);
+            // 
+            // deletebutton
+            // 
+            this.deletebutton.Location = new System.Drawing.Point(394, 382);
+            this.deletebutton.Name = "deletebutton";
+            this.deletebutton.Size = new System.Drawing.Size(79, 24);
+            this.deletebutton.TabIndex = 21;
+            this.deletebutton.Text = "Delete";
+            this.deletebutton.UseVisualStyleBackColor = true;
+            this.deletebutton.Click += new System.EventHandler(this.deletebutton_Click);
+            // 
+            // lastbutton
+            // 
+            this.lastbutton.Location = new System.Drawing.Point(489, 382);
+            this.lastbutton.Name = "lastbutton";
+            this.lastbutton.Size = new System.Drawing.Size(79, 24);
+            this.lastbutton.TabIndex = 22;
+            this.lastbutton.Text = "Last";
+            this.lastbutton.UseVisualStyleBackColor = true;
+            this.lastbutton.Click += new System.EventHandler(this.lastbutton_Click);
+            // 
+            // descriptiontextBox
+            // 
+            this.descriptiontextBox.Location = new System.Drawing.Point(607, 98);
+            this.descriptiontextBox.Multiline = true;
+            this.descriptiontextBox.Name = "descriptiontextBox";
+            this.descriptiontextBox.Size = new System.Drawing.Size(121, 214);
+            this.descriptiontextBox.TabIndex = 23;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.descriptiontextBox);
+            this.Controls.Add(this.lastbutton);
+            this.Controls.Add(this.deletebutton);
+            this.Controls.Add(this.loadbutton);
+            this.Controls.Add(this.firstbutton);
+            this.Controls.Add(this.Nextbutton);
+            this.Controls.Add(this.AddNewbutton);
+            this.Controls.Add(this.Savebutton);
+            this.Controls.Add(this.previousbutton);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pokemonpictureBox);
+            this.Controls.Add(this.Weighttextbox);
+            this.Controls.Add(this.HeighttextBox);
+            this.Controls.Add(this.HPtextBox);
+            this.Controls.Add(this.movetextBox);
+            this.Controls.Add(this.typetextBox);
+            this.Controls.Add(this.nametextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pokemonpictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox nametextBox;
+        private System.Windows.Forms.TextBox typetextBox;
+        private System.Windows.Forms.TextBox movetextBox;
+        private System.Windows.Forms.TextBox HPtextBox;
+        private System.Windows.Forms.TextBox HeighttextBox;
+        private System.Windows.Forms.TextBox Weighttextbox;
+        private System.Windows.Forms.PictureBox pokemonpictureBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button previousbutton;
+        private System.Windows.Forms.Button Savebutton;
+        private System.Windows.Forms.Button AddNewbutton;
+        private System.Windows.Forms.Button Nextbutton;
+        private System.Windows.Forms.Button firstbutton;
+        private System.Windows.Forms.Button loadbutton;
+        private System.Windows.Forms.Button deletebutton;
+        private System.Windows.Forms.Button lastbutton;
+        private System.Windows.Forms.TextBox descriptiontextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ JsonPokemon/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Collections;
+
+namespace JsonPokemon
+{
+    public partial class Form1 : Form
+    {
+        public int m_current = 0;
+        private ArrayList Pokes = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Savebutton_Click(object sender, EventArgs e)
+        {
+            var p = new Pokemon
+            {
+                name = nametextBox.Text,
+                type = typetextBox.Text,
+                move = movetextBox.Text,
+                HP = HPtextBox.Text,
+                Height = HeighttextBox.Text,
+                Weight = Weighttextbox.Text,
+                decrpition = descriptiontextBox.Text,
+                picture = pokemonpictureBox.ImageLocation
+            };
+
+          
+
+            Pokes.Add(p);
+            StreamWriter outfile = File.CreateText("Pokemon.JSON");
+            foreach (var item in Pokes)
+            {
+                outfile.WriteLine(JsonSerializer.Serialize(item));
+            }
+
+            outfile.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader inFile = File.OpenText("Pokemon.JSON");
+
+            while (inFile.Peek() != -1)
+            {
+                string pString = inFile.ReadLine();
+                Pokemon p = JsonSerializer.Deserialize<Pokemon>(pString);
+                Pokes.Add(p);
+            }
+            show();
+
+        }
+
+        public void show()
+        {
+            Pokemon p = (Pokemon)Pokes[m_current];
+            nametextBox.Text = p.name;
+            typetextBox.Text = p.type;
+            movetextBox.Text = p.move;
+            HPtextBox.Text = p.HP;
+            HeighttextBox.Text = p.Height;
+            Weighttextbox.Text = p.Weight;
+            descriptiontextBox.Text = p.decrpition;
+            pokemonpictureBox.ImageLocation = p.picture;
+
+        }
+
+        private void previousbutton_Click(object sender, EventArgs e)
+        {
+            if (m_current == 0)
+            {
+                m_current = Pokes.Count - 1;
+            }
+            else if (m_current > 0)
+            {
+                m_current--;
+            }
+            Show();
+
+        }
+
+        private void Nextbutton_Click(object sender, EventArgs e)
+        {
+            if (Pokes.Count == m_current + 1)
+            {
+                m_current = 0;
+            }
+            else if (m_current < Pokes.Count)
+            {
+                m_current++;
+            }
+            Show();
+
+        }
+
+        private void firstbutton_Click(object sender, EventArgs e)
+        {
+
+            Show();
+        }
+
+        private void lastbutton_Click(object sender, EventArgs e)
+        {
+            m_current = Pokes.Count - 1;
+            Show();
+
+        }
+        private void AddNewbutton_Click(object sender, EventArgs e)
+        {
+            save();
+            Clear();
+
+        }
+
+        private void loadbutton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deletebutton_Click(object sender, EventArgs e)
+        {
+            Pokes.Remove(Pokes[m_current]);
+            m_current =- 1;
+            Show();
+            save();
+
+        }
+        private void Clear()
+        {
+            nametextBox.Clear();
+            typetextBox.Clear();
+            movetextBox.Clear();
+            descriptiontextBox.Clear();
+            HPtextBox.Clear();
+            pokemonpictureBox.Image = null;
+        }
+        private void save()
+        {
+
+        }
+
+        private void pokemonpictureBox_Click(object sender, EventArgs e)
+        {
+        }
+    }
+
+}
